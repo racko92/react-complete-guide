b602454ec123e08c5cd98e8b61959e13f46062be
Vladimir Rackovic <vrackovic@execom.eu>
2018-03-22 13:28:38 +0100

    Update Lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -17,6 +17,22 @@ class Persons extends Component {
     console.log('[Persons.js] Inside componentDidMount');
   }
 
+  componentWillReceiveProps () {
+    console.log('[UPDATE Persons.js] Inside componentWillReceiveProps ()')
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('[UPDATE Persons.js] Inside shouldComponentUpdate ()', nextProps, nextState);
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    console.log('[UPDATE Persons.js] Inside componentWillUpdate ()', nextProps, nextState);
+  }
+
+  componentDidUpdate () {
+    console.log('[UPDATE Persons.js] Inside shouldComponentUpdate ()')
+  }
   render () {
     console.log('[Persons.js] Inside render()');
     return this.props.persons.map((person, index) => {
