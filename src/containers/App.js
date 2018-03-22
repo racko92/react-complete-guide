7ae88ee17556269826d1bd7abc3c75c3bc60dff3
Vladimir Rackovic <vrackovic@execom.eu>
2018-03-22 15:57:39 +0100

    Using setState Correctly (Async setState)

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
         { id: 23542345, name: 'Stephanie', age: 26 }
       ],
       otherState: 'some other value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0,
     }
   }
 
@@ -59,7 +60,12 @@ class App extends PureComponent {
   
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( {showPersons: !doesShow} );
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1,
+      }
+    } );
   }
 
   // PureComponent optimizing shouldComponentUpdate() by itself
